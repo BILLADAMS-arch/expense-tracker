d6f2b8343e9990ad33e30158fcad0d7bf15ec0a1
Billadams Nyamweno <nyamweno.billadams@gmail.com>
2025-04-15 09:38:49 +0300

    update on expense table

--- src/components/Expensetable.jsx
+++ src/components/Expensetable.jsx
@@ -1,25 +1,29 @@
 import React from 'react';
 
-const ExpenseTable = ({ expenses }) => {
+const ExpenseTable = ({ expenses, deleteExpense }) => {
   return (
     <table className="expense-table">
       <thead>
         <tr className="table-header">
-          <th className="table-data">Name</th>
-          <th className="table-data">Description</th>
-          <th className="table-data">Category</th>
-          <th className="table-data">Amount</th>
-          <th className="table-data">Date</th>
+          <th className="data">Name</th>
+          <th className="data">Description</th>
+          <th className="data">Category</th>
+          <th className="data">Amount</th>
+          <th className="data">Date</th>
+          <th className="data">Action</th>
         </tr>
       </thead>
       <tbody>
         {expenses.map((expense, index) => (
           <tr key={index} className="table-row">
-            <td className="table-data">{expense.name}</td>
-            <td className="table-data">{expense.description}</td>
-            <td className="table-data">{expense.category}</td>
-            <td className="table-data">{expense.amount}</td>
-            <td className="table-data">{expense.date}</td>
+            <td className="data">{expense.name}</td>
+            <td className="data">{expense.description}</td>
+            <td className="data">{expense.category}</td>
+            <td className="data">{expense.amount}</td>
+            <td className="data">{expense.date}</td>
+            <td className="data">
+              <button onClick={() => deleteExpense(index)} className="delete-button">Delete</button>
+            </td>
           </tr>
         ))}
       </tbody>
